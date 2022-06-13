05fc3c6739f05d3f47e2cd4ef6c02e0f364b0a84
Roman Shulenkov <roman.shulenkov@shuracore.com>
2022-06-13 22:14:09 +0300

    chore: start to implement the verilog-ethernet

--- jetson/repo/yocto.sh
+++ jetson/repo/yocto.sh
@@ -9,29 +9,28 @@ then
     
     #setup env
     . ./setup-env --machine jetson-nano-devkit
+        
+    #check current state
+    bitbake-layers show-layers
     
-    #copy config files
-    cp /home/user/local.conf /home/user/tegra-demo-distro/build/conf/local.conf
-    cp /home/user/bblayers.conf /home/user/tegra-demo-distro/build/conf/bblayers.conf        
-    
-    #qt5
-    #bitbake
-    bitbake-layers add-layer /home/user/tegra-demo-distro/layers/meta-qt5
-    
+    #build
+    bitbake demo-image-full    
 else
     echo "Clone tegra-demo-distro" 
     
     #git clone tegra-demo-distro
     git clone https://github.com/OE4T/tegra-demo-distro.git
     cd tegra-demo-distro/
-    git checkout efc6c4d4db48aaa3029d4d74acd 22bde29c9b0055b
-    #hardknott
+
+    #kirkstone
+    git checkout kirkstone
     git submodule update --init
     
     cd layers/
     git clone https://github.com/meta-qt5/meta-qt5.git
     cd meta-qt5
-    git checkout a00af3eae082b772469d9dd21b2371dd4d237684
+    #kirkstone
+    git checkout kirkstone
     
     cd ../../
     
@@ -39,25 +38,16 @@ else
     . ./setup-env --machine jetson-nano-devkit
 
     #bitbake
-    bitbake-layers show-layers    
-    bitbake demo-image-full
-    
-    cd /home/user/tegra-demo-distro/build
-    
-    #bitbake
-    bitbake-layers add-layer /home/user/tegra-demo-distro/layers/meta-qt5    
+    echo "Add qt5-meta layer" 
+    #check current state
     bitbake-layers show-layers
-    bitbake qtbase        
-    bitbake meta-toolchain-qt5
-    
-    #copy config files
-    cp /home/user/local.conf /home/user/tegra-demo-distro/build/conf/local.conf
-    cp /home/user/bblayers.conf /home/user/tegra-demo-distro/build/conf/bblayers.conf        
-    
+    #add qt5
+    bitbake-layers add-layer /home/user/tegra-demo-distro/layers/meta-qt5    
+    #check state
+    bitbake-layers show-layers
+    #build
+    bitbake demo-image-full
 fi
-
-bitbake-layers show-layers
-bitbake demo-image-full
 
 if [ ! -d "/home/user/artifact" ] 
 then
@@ -71,4 +61,6 @@ cd /home/user/tegra-demo-distro/build/tmp/deploy/images/jetson-nano-devkit
 cp demo-image-full-jetson-nano-devkit.tegraflash.tar.gz /home/user/artifact
 cd /home/user/artifact
 tar xzvf demo-image-full-jetson-nano-devkit.tegraflash.tar.gz
+
+echo "Completed" 
 
